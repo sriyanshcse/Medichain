1ac64885e7d2c7f5b0a0f384f4c0772bffbbbb38
Sriyansh <sriyansh.cse@gmail.com>
2018-02-28 01:33:02 +0530

    removed other unused directories

--- fabcar/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker rm -f $(docker ps -aq)
-docker network prune
-docker rmi dev-peer0.org1.example.com-fabcar-1.0-5c906e402ed29f20260ae42283216aa75549c571e2e380f3615826365d8269ba
